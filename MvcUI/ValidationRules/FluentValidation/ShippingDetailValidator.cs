7bb55042393f4118d63fe3ad81603ef4f72ff810
Zekican Kayaoğlu <78613397+zekicankayaoglu@users.noreply.github.com>
2023-10-04 19:41:28 +0300

    Add files via upload

---
+++ MvcUI/ValidationRules/FluentValidation/ShippingDetailValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MvcUI.Models;
+
+namespace MvcUI.ValidationRules.FluentValidation
+{
+    public class ShippingDetailValidator : AbstractValidator<RegisterDto>
+    {
+        public ShippingDetailValidator()
+        {
+            RuleFor(expression: s => s.FirstName).NotEmpty();
+            RuleFor(expression: s => s.FirstName).MinimumLength(2);
+            RuleFor(expression: s => s.LastName).NotEmpty();
+            RuleFor(expression: s => s.Password).NotEmpty();
+            RuleFor(expression: s => s.City).NotEmpty();
+            RuleFor(expression: s => s.Email).NotEmpty();
+
+
+            //RuleFor(expression: s => s.City).NotEmpty().When(predicate:s => s.Age < 18);
+            // RuleFor(expression: s => s.Address).Must(StartsWithA);
+        }
+
+        private bool StartsWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
---
+++ MvcUI/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MvcUI.Models;
+
+namespace MvcUI.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private ICategoryService _categoryService;
+
+        public CategoryListViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public ViewViewComponentResult Invoke()
+        {
+            var model = new CategoryListViewModel
+            {
+                Categories = _categoryService.GetAll(),
+                CurrentCategory = Convert.ToInt32(HttpContext.Request.Query["Category"])
+            };
+            return View(model);
+        }
+    }
+}
